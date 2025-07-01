e002073a7d00816f4efeb0d70739ccb1f67fb088
Maja Subasic <89577755+Maja0110@users.noreply.github.com>
2025-07-01 15:10:35 +0200

    Create buildgradle.kts

---
+++ buildgradle.kts
@@ -0,0 +1,11 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.10.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
